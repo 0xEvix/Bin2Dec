0daffed87017a939937af8675346c04bb482e195
0xEvix <envini.tr@outlook.com>
2023-02-06 10:07:48 +0100

    rules

---
+++ main.py
@@ -0,0 +1,17 @@
+# le Binaire est le système de nombre utilisé par tous les ordinateurs => Important de comprendre le Binaire
+# but de Bin2Dec => pratiquer & comprendre comment fonctionne les calcules binaires 
+
+# Bin2Dec permet :
+#   - à l'utilisateur d'entrer un string de 8 chiffre binaires => des 0 & des 1 
+#   - dans nimporte quel ordre
+#   - et Affiche son équivalent décimal
+
+# 2 contraintes : 
+#   - Tableaux ne peuvent pas être utilisés pour contenir les chiffres binaires entrés par l'utilisateur
+#   - Déterminer le nombre décimal équivalent d'un nombre binaire particulier dans la séquence doit être calculé en utilisant une seule fonction
+#       mathématique (ex : Le logarithme naturel) => A moi de décider quelle fonction utiliser
+
+# 1) Utilisateur peut entrer 8 chiffres binaires dans un champs d'entrée
+# 2) Utilisateur doit être notifié s'il rentre autre chose que des 0 & des 1
+# 3) Utilisateur voit le résultat dans un seul champs de sortie content le décimal (base 10) équivalent au nombre binaire qui a été entré
+# 4) Utilisateur peut entrer un nombre variable de chiffre binaire
